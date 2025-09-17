7e357174a10f77ed0ebb4f3818d9e146c0c9b9d5
schhaohao <sunchenhao518@163.com>
2025-09-17 11:33:34 +0800

    init

---
+++ src/main/java/com/sch/demo/demo2cicd/Demo2CiCdApplication.java
@@ -0,0 +1,13 @@
+package com.sch.demo.demo2cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2CiCdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2CiCdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sch/demo/demo2cicd/Demo2CiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sch.demo.demo2cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2CiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
